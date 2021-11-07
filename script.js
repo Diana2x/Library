a06769667535c77b651dfec4b97be5642b6b08bd
diana2x <dianaclxcoe@gmail.com>
2021-11-07 14:56:49 +0100

    script work

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ class Book{
 function addNewBook(title, author, pages, status){
         myLibrary.push(new Book(title, author, pages, status));
 }
-console.log(myLibrary);
+
+function findBook(array, title){
+    if(array.length === 0 || array.length === null){
+        return;
+    }
+    for(book of array)
+        if(book.title === title){
+            return array.indexOf(book);
+        }
+}
+
+function removeBook(book){
+    const deletebook = findBook(book);
+    myLibrary.splice(deletebook)
+}
 addNewBook("Harry Potter", "j.k roling", 27, false);
+addNewBook("The Lord of the rings", "j.k roling", 27, false);
+addNewBook("The man in the high castle", "j.k roling", 27, false);
 console.log(myLibrary);
+
+console.log(findBook(myLibrary, "The Lord of the rings"));
